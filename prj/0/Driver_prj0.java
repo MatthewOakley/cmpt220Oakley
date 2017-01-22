92b91e3e86436e270628030abf61b27787e7922c
matt oakley <matt oakley>
2017-01-22 13:29:58 -0500

    This is project 0

---
+++ prj/0/Driver_prj0.java
@@ -0,0 +1,18 @@
+/**
+ * file: Driver_prj0.java 
+ * author: Matthew Oakley 
+ * course: CMPT 220-201
+ * assignment: project 0  
+ * due date: January 24, 2017   
+ * version: 1  
+ *
+ * This files displays the text 'Hello World!' 
+ * to the console.
+ */
+
+
+public class Driver_prj0 {
+  public static void main(String [] args) {
+    System.out.println("Hello World!");
+  }
+}

67869ba3c7a30d1b0de4605aa77964210afd817e
matt oakley <matt oakley>
2017-02-21 17:21:18 -0500

    This is problem 5.12 with better comments

--- labs/3/Problem3.java
+++ labs/3/Problem3.java
@@ -18,21 +18,29 @@ public class Problem3 {
     // will run until there is no remainder for sqrt of num
     while(true) {
       num++;
-      
       // if the sqrt has a remainder will not break
       if(!remainder(Math.sqrt(num))) {
         n = (int)Math.sqrt(num);
         break;
       }
-      
     }
     
     System.out.println("The smallest value of n so that n^2 " 
     + "> 12,000: " + n);
   }
-  
-  // this takes in a double and will return a boolean the
-  // boolean will be true for remainder or false for no remainder
+
+  /**
+   * remainder
+   * 
+   * This function will check if a number has a remainder given
+   * the input it gets remainder is anything after the decimal
+   * place.
+   * Parameters:
+   *  number: the number which will be checked if it has a 
+   *              remainder
+   * Return value: will return true if it has a remainder
+   *                    false if it has no remainder
+   */
   static boolean remainder(double number) {
     // no remainder
     if(number - ((int)(number)) == 0)

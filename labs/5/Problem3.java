5d54a5d79c2e35e20e9222cb459b750fe37a0d05
matt oakley <matt oakley>
2017-03-25 12:10:58 -0400

    base

---
+++ labs/5/Problem3.java
@@ -0,0 +1,19 @@
+/**
+ * file: Problem2.java 
+ * author: Matthew Oakley 
+ * course: CMPT 220-201
+ * assignment: problem 7.31
+ * due date: March 28, 2017   
+ * version: 1  
+ *
+ * This program sorts using selection sort largest to smallest
+ */
+
+import java.util.Scanner;
+
+public class Problem3{
+  public static void main(String[] args){
+    
+    Scanner input = new Scanner(System.in);
+  }
+}

91a2fe621beccda3e81a3eb373bd655b78f1011d
MatthewOakley <matthew.oakley1@marist.edu>
2017-04-16 12:33:35 -0400

    added problem file

---
+++ Problem2.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class Problem2{
+  public static void main(String[] args){
+    
+    // sets up scanner for user input
+    Scanner input = new Scanner(System.in);
+    
+    // sets up location object
+    Location location = new Location();
+    
+    // asks user for row and column and sets it
+    System.out.print("Enter the number of rows and columns in the array: ");
+    int row = input.nextInt();
+    int column = input.nextInt();
+    
+    // gets all the array numbers
+    System.out.println("Enter the array: ");
+    double[][] array = new double[3][4];
+    
+    // user inputs 2D array
+    for(int i = 0; i < row; i++){
+      for(int j = 0; j < column; j++){
+        array[i][j] = input.nextDouble();
+      }
+    }
+    
+    // finds the location of largest value
+    location.locateLargest(array);
+    
+    // displays location of largest value
+    System.out.println("The location of the largest element is " 
+        + location.maxValue + " at (" + location.row + ", " + location.column + ")");
+  }
+}

9d5f85b41e7af0f0cffec16e965f11428701a281
matt oakley <matt oakley>
2017-04-20 23:07:22 -0400

    Displays the first 10 numbers and includes comments

---
+++ labs/7/Problem3.java
@@ -0,0 +1,39 @@
+/**
+ * file: Problem3.java 
+ * author: Matthew Oakley 
+ * course: CMPT 220-201
+ * assignment: problem 10.17
+ * due date: April 25, 2017   
+ * version: 1  
+ *
+ * This will find the first 10 square numbers after long.MAX_VALUE
+ */
+
+import java.math.BigInteger; 
+ 
+public class Problem3{
+  public static void main(String[] args){
+    
+    // sets long max value to a string
+    String longMax = Long.toString(Long.MAX_VALUE);
+    
+    // makes n set to maximum long value
+    BigInteger n = new BigInteger(longMax);
+    
+    // makes an array of BigIntegers so the squares can be stored
+    BigInteger[] nums = new BigInteger[10];
+    
+    // will get the first 10 square numbers and save them to the array
+    for(int q = 0; q < 10; q++){
+      // add one to big integer number
+      n = n.add(BigInteger.ONE);
+      
+      // does the square of the big int
+      nums[q] = n.pow(2);
+    }
+    
+    // displays the 10 squared numbers
+    for(int i = 0; i < 10; i++)
+      System.out.println( (i + 1) + ": " + nums[i]);
+  }
+}
